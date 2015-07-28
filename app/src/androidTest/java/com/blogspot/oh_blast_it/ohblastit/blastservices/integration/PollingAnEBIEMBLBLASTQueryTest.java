7653e70c2e93bc5893c0af2b8e3c38960a5cc217
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-07-28 20:32:04 +0100

    Removed unused imports

--- app/src/androidTest/java/com/blogspot/oh_blast_it/ohblastit/blastservices/integration/PollingAnEBIEMBLBLASTQueryTest.java
+++ app/src/androidTest/java/com/blogspot/oh_blast_it/ohblastit/blastservices/integration/PollingAnEBIEMBLBLASTQueryTest.java
@@ -1,13 +1,7 @@
 package com.blogspot.oh_blast_it.ohblastit.blastservices.integration;
 
-import android.content.Context;
-import android.test.InstrumentationTestCase;
-
-import com.blogspot.oh_blast_it.ohblastit.blastservices.BLASTSearchEngine;
 import com.blogspot.oh_blast_it.ohblastit.blastservices.EMBLEBIBLASTService;
-import com.blogspot.oh_blast_it.ohblastit.blastservices.SearchStatus;
-import com.blogspot.oh_blast_it.ohblastit.domain.BLASTQuery;
-import com.blogspot.oh_blast_it.ohblastit.testhelpers.SendBLASTQuery;
+
 
 import java.util.concurrent.ExecutionException;
 
--- app/src/androidTest/java/com/blogspot/oh_blast_it/ohblastit/blastservices/integration/PollingAnNCBIBLASTQueryTest.java
+++ app/src/androidTest/java/com/blogspot/oh_blast_it/ohblastit/blastservices/integration/PollingAnNCBIBLASTQueryTest.java
@@ -1,15 +1,6 @@
 package com.blogspot.oh_blast_it.ohblastit.blastservices.integration;
 
-import android.content.Context;
-import android.test.InstrumentationTestCase;
-
-import com.blogspot.oh_blast_it.ohblastit.blastservices.BLASTSearchEngine;
 import com.blogspot.oh_blast_it.ohblastit.blastservices.NCBIBLASTService;
-import com.blogspot.oh_blast_it.ohblastit.blastservices.SearchStatus;
-import com.blogspot.oh_blast_it.ohblastit.domain.BLASTQuery;
-import com.blogspot.oh_blast_it.ohblastit.testhelpers.SendBLASTQuery;
-
-import java.util.concurrent.ExecutionException;
 
 import static com.blogspot.oh_blast_it.ohblastit.testhelpers.BLASTQueryBuilder.validPendingNCBIBLASTQuery;
 
